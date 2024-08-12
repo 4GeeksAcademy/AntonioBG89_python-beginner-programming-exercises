5abd6a336277a7fcec0116101be66da596fae9be
Antonio <116432648+AntonioBG89@users.noreply.github.com>
2024-08-12 18:52:00 +0000

    Finalizado

--- exercises/15-Looping-With-FizzBuzz/app.py
+++ exercises/15-Looping-With-FizzBuzz/app.py
@@ -1,5 +1,12 @@
 def fizz_buzz():
     # ✅↓ Write your code here ↓✅
-
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            i = 'FizzBuzz'
+        elif i % 5 == 0:
+            i = 'Buzz'
+        elif i % 3 == 0:
+            i = 'Fizz'
+        print(i)
 # ❌↓ DON'T CHANGE THE CODE BELOW ↓❌
 fizz_buzz()
--- exercises/18-The-Beatles/app.py
+++ exercises/18-The-Beatles/app.py
@@ -1,2 +1,12 @@
 # ✅↓ Write your code here ↓✅
+def sing():
+    str = ''
+    for i in range(4):
+        str += 'let it be,\n'
+    str += 'there will be an answer,\n'
+    for i in range(5):
+        str += 'let it be,\n'
+    str += 'whisper words of wisdom, let it be'
+    return str
 
+print(sing())
